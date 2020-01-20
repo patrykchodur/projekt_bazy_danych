f529c8edeb9e1503be25b5301ba28745d0cb6b92
Patryk Chodur <patrykchodur@icloud.com>
2020-01-20 21:34:03 +0100

    compile.sh doesnt run if compilation was unsuccessful

--- compile.sh
+++ compile.sh
@@ -6,4 +6,6 @@ export PATH_TO_FX=./lib/javafx-sdk-13.0.2/lib
 
 javac --module-path $PATH_TO_FX --add-modules javafx.controls --add-modules javafx.fxml,javafx.base Project/Project.java
 
-java --module-path $PATH_TO_FX --add-modules javafx.controls --add-modules javafx.fxml,javafx.base Project.Project
+if [ $? == 0 ]; then
+	java --module-path $PATH_TO_FX --add-modules javafx.controls --add-modules javafx.fxml,javafx.base Project.Project
+fi
