fd373901d2e686f4e47b1e854f90e91dff589869
Patryk Chodur <7chodur@fis.agh.edu.pl>
2020-01-23 21:11:14 +0100

    added delete_tables

---
+++ sql/delete_tables.sql
@@ -0,0 +1,9 @@
+DROP TABLE "aktywnosc";
+DROP TABLE "donosy";
+DROP TABLE "ministerstwo";
+DROP TABLE "myslozbrodnie";
+DROP TABLE "obywatel";
+DROP TABLE "obywatel_aktywnosc";
+DROP TABLE "obywatel_rozmowa";
+DROP TABLE "praca";
+DROP TABLE "rozmowa";
