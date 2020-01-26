0ada2990a5417e4739629e551a83ab5e6787ec4d
Patryk Chodur <patrykchodur@icloud.com>
2020-01-26 20:18:09 +0100

    added ministries in init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,2 +1,59 @@
-INSERT INTO Obywatele (imie, nazwisko, data_urodzenia, plec, czlonkowstwo_partii) VALUES ('admin', 'admin', '19971211', 'M', 'wewnetrznaPartia');
+INSERT INTO Obywatele (
+	imie,
+	nazwisko,
+	data_urodzenia,
+	plec,
+	czlonkowstwo_partii
+)
+VALUES (
+	'admin',
+	'admin',
+	'19971211',
+	'M',
+	'wewnetrznaPartia'
+);
+
+INSERT INTO Ministerstwo (
+	nazwa_ministerstwa,
+	liczba_pracownikow,
+	opis_ministerstwa
+)
+VALUES (
+	'Ministerstwo Milosci',
+	0,
+	'Ministerstwo zajmujace sie leczeniem serc obywateli'
+);
+
+INSERT INTO Ministerstwo (
+	nazwa_ministerstwa,
+	liczba_pracownikow,
+	opis_ministerstwa
+)
+VALUES (
+	'Ministerstwo Prawdy',
+	0,
+	'Ministerstwo stojace na strazy prawdy i dbajace, aby byla ona zawsze aktualna'
+);
+
+INSERT INTO Ministerstwo (
+	nazwa_ministerstwa,
+	liczba_pracownikow,
+	opis_ministerstwa
+)
+VALUES (
+	'Ministerstwo Pokoju',
+	0,
+	'Ministerstwo zajmujace sie walka o pokoj na swiecie'
+);
+
+INSERT INTO Ministerstwo (
+	nazwa_ministerstwa,
+	liczba_pracownikow,
+	opis_ministerstwa
+)
+VALUES (
+	'Ministerstwo Obfitosci',
+	0,
+	'Ministerstwo zajmujace sie planowaniem produkcji i dystrybucja jej owocow'
+);
 
