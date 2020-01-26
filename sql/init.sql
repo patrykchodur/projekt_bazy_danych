f4bb09e595633ece6b3bbf33c2af22dfa7bc58b7
Patryk Chodur <7chodur@fis.agh.edu.pl>
2020-01-26 20:23:36 +0100

    small sql changes from pascal

--- sql/init.sql
+++ sql/init.sql
@@ -1,2 +1,4 @@
-INSERT INTO Obywatele (imie, nazwisko, data_urodzenia, plec, czlonkowstwo_partii) VALUES ('admin', 'admin', '19971211', 'M', 'wewnetrznaPartia');
+INSERT INTO obywatel (imie, nazwisko, data_urodzenia, plec, czlonkowstwo_partii) VALUES ('admin', 'admin', '19971211', 'M', 'wewnetrznaPartia');
+
+INSERT INTO DaneLogowania (id_obywatela, nick, haslo) SELECT o.id, 'admin', 'admin' FROM Obywatel o WHERE o.imie = 'admin';
 
