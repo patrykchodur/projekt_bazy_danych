fda086c70f7dd0d8a2829144670fcac61f21d092
Patryk Chodur <7chodur@fis.agh.edu.pl>
2020-01-29 18:35:54 +0100

    added trigger for tracking workers in ministery

--- sql/views_and_triggers.sql
+++ sql/views_and_triggers.sql
@@ -19,3 +19,38 @@ $$ LANGUAGE 'plpgsql';
 
 CREATE TRIGGER ocena_trigger AFTER INSERT ON myslozbrodnie
 FOR EACH ROW EXECUTE PROCEDURE ocena_po_dodaniu_myslozbrodni();
+
+CREATE OR REPLACE FUNCTION ilosc_pracownikow() RETURNS TRIGGER AS $$
+	DECLARE
+		id_wczesniejszego_ministerstwa INTEGER;
+		id_nowego_ministerstwa INTEGER;
+	BEGIN
+		IF TG_OP = 'UPDATE' OR TG_OP = 'DELETE' THEN
+			IF OLD.praca_id IS NULL THEN
+				id_wczesniejszego_ministerstwa := NULL;
+			ELSE
+				SELECT p.ministerstwo_id INTO id_wczesniejszego_ministerstwa FROM
+				praca p WHERE id = OLD.praca_id;
+			END IF;
+
+			IF id_wczesniejszego_ministerstwa IS NOT NULL THEN
+				UPDATE ministerstwo SET liczba_pracownikow = liczba_pracownikow - 1
+				WHERE id = id_wczesniejszego_ministerstwa;
+			END IF;
+		END IF;
+
+		IF TG_OP = 'UPDATE' OR TG_OP = 'INSERT' THEN
+			SELECT p.ministerstwo_id INTO id_nowego_ministerstwa FROM
+			praca p WHERE id = NEW.praca_id;
+
+			UPDATE ministerstwo SET liczba_pracownikow = liczba_pracownikow + 1
+			WHERE id = id_nowego_ministerstwa;
+			RETURN NEW;
+		END IF;
+		RETURN NULL;
+	END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE TRIGGER ilosc_pracownikow_trigger AFTER INSERT OR UPDATE OR DELETE ON obywatel
+FOR EACH ROW EXECUTE PROCEDURE ilosc_pracownikow();
+
